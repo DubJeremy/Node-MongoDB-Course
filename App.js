19cdd1695404b022f7661367f940f35b145a3663
DubJ <jeremy.ah33@gmail.com>
2022-01-24 17:04:13 +0100

    bodyParser obsolete

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const Thing = require('./models/Thinks');
 
 mongoose.connect('mongodb+srv://DubJeremy:MotdePassemongoDBsecu66@cluster0.utafe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -12,6 +10,8 @@ mongoose.connect('mongodb+srv://DubJeremy:MotdePassemongoDBsecu66@cluster0.utafe
 
 const app =express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 app.post('/api/stuff', (req, res, next) => {
   delete req.body._id;
@@ -45,13 +45,13 @@ app.delete('/api/stuff/:id', (req, res, next) => {
 
 app.get('/api/stuff/:id', (req, res, next) => {
   Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json((thing)))
+    .then(thing => res.status(200).json(( thing )))
     .catch(error => res.status(404).json({ error }));
 });
 
-app.get('/api/stuff', (req, res, next) => {
+app.use('/api/stuff', (req, res, next) => {
   Thing.find()
-    .then(things => res.status(200).json((things)))
+    .then(things => res.status(200).json(( things )))
     .catch(error => res.status(400).json({ error }));
 });
 
