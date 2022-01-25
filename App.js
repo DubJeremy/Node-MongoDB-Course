93d45d3912c88156eb888424a0f6008b0c49f6c6
DubJ <jeremy.ah33@gmail.com>
2022-01-25 18:12:11 +0100

    organize the backend

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Thing = require('./models/Thinks');
+
+const stuffRoutes = require('./routes/stuffRoute');
 
 mongoose.connect('mongodb+srv://DubJeremy:MotdePassemongoDBsecu66@cluster0.utafe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,38 +23,6 @@ app.use((req, res, next) => {
 
 // app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id})
-    .then(() => res.status(200).json({ message: "Objet modifié !"}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !"}))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(( thing )))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(( things )))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
