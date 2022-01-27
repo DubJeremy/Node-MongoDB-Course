dee193935cd2c1eaf6429593bfd9c83e56151959
DubJ <jeremy.ah33@gmail.com>
2022-01-27 11:55:40 +0100

    delete security

--- controllers/stuffController.js
+++ controllers/stuffController.js
@@ -17,6 +17,19 @@ exports.modifyThing = (req, res, next) => {
 };
 
 exports.deleteThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then((thing) => {
+            if (!thing) {
+                return res.status(404).json({
+                    error: new Error('Objet non trouvé !')
+                });
+            }
+            if (thing.userId !== req.auth.userId) {
+                return res.status(400).json({
+                    error: new Error('Requête non autorisée !')
+                })
+            }
+        })
     Thing.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: "Objet supprimé !"}))
         .catch(error => res.status(404).json({ error }));
